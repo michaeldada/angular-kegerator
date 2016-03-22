3b2deb0658bf4cf773dc37c7d2cac45f55362ea3
Alex Fallenstedt and Mike Dada <a.fallenstedt@gmail.com>
2016-03-22 09:45:43 -0700

    add event emitter for click

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,12 +4,32 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
   <div class = "container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>To-Do List</h1>
+    <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+      {{ task.description }}
+    </h3>
   </div>
   `
-
 })
 
 export class AppComponent {
+  public tasks: Task[];
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("More Breakfast Sandwiches", 2),
+      new Task("Do the laundry", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
