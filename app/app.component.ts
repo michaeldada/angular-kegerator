e66c628c81faa4687a9047730aab4e1068cdc724
Alex Fallenstedt and Mike Dada <mail.michaeldada@gmail.com>
2016-03-22 10:30:31 -0700

    add more components to task

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,58 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+@Component({
+    selector: 'task-display',
+    inputs: ['task'],
+  template: `
+  <h3>{{ task.description }}</h3>
+  `
+})
+
+export class TaskComponent {
+  public task: Task;
+}
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent],
+  template: `
+    <task-display *ngFor="#currentTask of taskList"
+      (click)="taskClicked(currentTask)"
+      [class.selected]="currentTask === selectedTask"
+      [task]="currentTask">
+    </task-display>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor(){
+    this.onTaskSelect = new EventEmitter();
+  }
+
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
+
 
 @Component({//This component is special - it will hold our entire app. An Angular2 app is built with a tree of components. We have one component called the root component that is responsible for loading its child components, and each of them has their own child components. This way we break our app into manageable chunks.
   selector: 'my-app',
-  template: `
-  <div class = "container">
-    <h1>To-Do List</h1>
-    <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
-      {{ task.description }}
-    </h3>
-  </div>
-  `
+    directives: [TaskListComponent],
+    template: `
+      <div class="container">
+        <h1>To-Do List</h1>
+        <task-list
+         [taskList]="tasks"
+         (onTaskSelect)="taskWasSelected($event)">
+        </task-list>
+      <div>
+    `
 })
 
 export class AppComponent {
